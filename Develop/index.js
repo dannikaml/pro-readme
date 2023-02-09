e8e14dba14401024432a8963d5612051585108bb
dannikaml <dannikaml@hotmail.com>
2023-02-09 12:06:36 -0600

    add package.json/lock; add array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is the title of your project?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a description of your project!',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions!',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What should users know about contributing to this repository?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please pick a license from the list provided!',
+    choices: ['MIT', 'GPL 3.0', 'APACHE 2.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
